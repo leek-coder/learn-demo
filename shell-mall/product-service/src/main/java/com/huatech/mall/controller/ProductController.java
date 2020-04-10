3ba8540d4bbd0101b1a681ddcfe2218aeb1893ca
like <13521819338@163.com>
2020-04-10 13:45:46 +0800

    [UPD]测试冲突

--- shell-mall/product-service/src/main/java/com/huatech/mall/controller/ProductController.java
+++ shell-mall/product-service/src/main/java/com/huatech/mall/controller/ProductController.java
@@ -111,6 +111,7 @@ public class ProductController {
     @GetMapping(value = "/demo02")
     public ResponseResult demo2() {
         log.info("demo02");
+        log.info("测试冲突");
         return ResponseResult.success("demo2");
     }
 
