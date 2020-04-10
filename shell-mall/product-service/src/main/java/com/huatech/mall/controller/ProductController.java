1209d8d39deba87f2ba69fe8352b9aaeaa82a71a
like <13521819338@163.com>
2020-04-10 13:49:48 +0800

    [ADD]测试提交02

--- shell-mall/product-service/src/main/java/com/huatech/mall/controller/ProductController.java
+++ shell-mall/product-service/src/main/java/com/huatech/mall/controller/ProductController.java
@@ -112,7 +112,7 @@ public class ProductController {
     public ResponseResult demo2() {
         log.info("我是leek");
         log.info("demo02");
-        log.info("测试冲突leek");
+        log.info("测试冲突0002");
         return ResponseResult.success("demo2");
     }
 
